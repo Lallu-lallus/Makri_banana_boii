d24d2725db413ae29aca0d9dbfe6fd1f4f8fdf40
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-11-23 14:38:00 +0530

    Banana 🍌

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ logging.getLogger().setLevel(logging.ERROR)
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from utils import Media
-from info import SESSION, API_ID, API_HASH, BOT_TOKEN
+from makkiri import SESSION, API_ID, API_HASH, BOT_TOKEN
 import pyromod.listen
 
 class Bot(Client):
