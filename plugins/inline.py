207af8e79b2285f13bf2e54c1ff0ca60eb5feb6e
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-11-23 14:42:02 +0530

    stickeroli

--- plugins/inline.py
+++ plugins/inline.py
@@ -3,7 +3,7 @@ from pyrogram import Client, emoji, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultCachedDocument
 
 from utils import get_search_results, is_subscribed, get_size
-from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
+from makkiri import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
 
 logger = logging.getLogger(__name__)
 cache_time = 0 if AUTH_USERS or AUTH_CHANNEL else CACHE_TIME
