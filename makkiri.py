be7126dce5d902f48e44e836adfc2ad0c911b621
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-11-23 19:28:44 +0530

    Update makkiri.py

--- makkiri.py
+++ makkiri.py
@@ -14,7 +14,7 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 
 # Admins, Channels & Users
-OWNERS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS'].split()]
+BANANA_BOI = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS'].split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS'].split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
