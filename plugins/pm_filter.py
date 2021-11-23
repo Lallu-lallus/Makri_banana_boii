adc50a158788d50f4b0e375fd897d8039f5ed68c
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-11-23 14:42:53 +0530

    Ultra Pro Maxx Vazha Of all time

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1,5 +1,5 @@
-#Kanged From @TroJanZheX
-from info import AUTH_CHANNEL, AUTH_USERS, CUSTOM_FILE_CAPTION, API_KEY, AUTH_GROUPS
+#Kanged From @Makkiri Banana 🍌
+from makkiri import AUTH_CHANNEL, AUTH_USERS, CUSTOM_FILE_CAPTION, API_KEY, AUTH_GROUPS
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram import Client, filters
 import re
