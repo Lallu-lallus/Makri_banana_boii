bb924b4944f3b043a57ea13bdf200a0fc5598123
PANDITHAN <91892960+PANDITHAN@users.noreply.github.com>
2021-11-25 17:13:33 +0530

    Yt

--- plugins/commands.py
+++ plugins/commands.py
@@ -241,7 +241,7 @@ async def bot_info(bot, message):
         ]
     await message.reply(text="help text", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
 
-@Client.on_message(filters.command('about'))
+@Client.on_message(filters.command('yt'))
 async def bot_info(bot, message):
     buttons = [
         [
