8d98da46d1e02e2681b3f7c877f3a1ad3bf2cc1f
PANDITHAN <91892960+PANDITHAN@users.noreply.github.com>
2021-11-25 17:18:17 +0530

    🤧

--- plugins/commands.py
+++ plugins/commands.py
@@ -222,24 +222,6 @@ async def delete(bot, message):
         await msg.edit('File is successfully deleted from database')
     else:
         await msg.edit('File not found in database')
-@Client.on_message(filters.command('code'))
-async def bot_info(bot, message):
-    buttons = [
-        [
-            InlineKeyboardButton('Update Channel', url='https://t.me/M_STER_TECH')
-        ]
-        ]
-    await message.reply(text="BOT REPO 👇", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
-
-@Client.on_message(filters.command('help'))
-async def bot_info(bot, message):
-    buttons = [
-        [
-            InlineKeyboardButton('Update Channel', url='https://t.me/M_STER_TECH'),
-            InlineKeyboardButton('Source Code', url='https://github.com/Lallu-lallus/ALPHA-AUTO-FILTER-BOT')
-        ]
-        ]
-    await message.reply(text="help text", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
 
 @Client.on_message(filters.command('yt'))
 async def bot_info(bot, message):
@@ -248,4 +230,13 @@ async def bot_info(bot, message):
             InlineKeyboardButton('Update Channel', url='https://t.me/M_STER_TECH')
         ]
         ]
-    await message.reply(text="about text text", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+    await message.reply(text="yt text", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+
+@Client.on_message(filters.command('help'))
+async def bot_info(bot, message):
+    buttons = [
+        [
+            InlineKeyboardButton('Update Channel', url='https://t.me/M_STER_TECH')
+        ]
+        ]
+    await message.reply(text="help text", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
