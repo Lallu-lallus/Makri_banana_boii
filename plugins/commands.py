77c52ddae224f063ba62d935f55fbacfddd6f956
CLÆ͜͡Ｙ <78960870+CLaY9950@users.noreply.github.com>
2021-11-25 20:21:22 +0530

    🤒🤷

--- plugins/commands.py
+++ plugins/commands.py
@@ -232,7 +232,7 @@ async def bot_info(bot, message):
     await message.reply(text="yt text", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
 
 @Client.on_message(filters.command('about'))
-async def bot_info(bot, message):
+async def bot_info_m(bot, message):
     buttons = [
         [
             InlineKeyboardButton('Update Channel', url='https://t.me/M_STER_TECH')
