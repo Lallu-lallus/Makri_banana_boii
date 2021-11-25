a74ac4f05bf8ee0f0f4ea098d77a4da908a361bc
PANDITHAN <91892960+PANDITHAN@users.noreply.github.com>
2021-11-25 20:11:25 +0530

    🥶

--- plugins/commands.py
+++ plugins/commands.py
@@ -230,3 +230,13 @@ async def bot_info(bot, message):
         ]
         ]
     await message.reply(text="yt text", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+
+@Client.on_message(filters.command('about'))
+async def bot_info(bot, message):
+    buttons = [
+        [
+            InlineKeyboardButton('Update Channel', url='https://t.me/M_STER_TECH')
+        ]
+        ]
+    await message.reply(text="yt text", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+
